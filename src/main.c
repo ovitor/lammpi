07e9ff1067475d32e320ff28a1451ee0845fa6a1
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2015-10-06 15:26:26 -0300

    fixing MPI rank and size calls

--- src/main.c
+++ src/main.c
@@ -12,8 +12,8 @@ int main(int argc, char *argv[]) {
   int rank, size;
 
   MPI_Init(&argc, &argv);
-  MPI_Common_rank(MPI_COMM_WORLD, &rank);
-  MPI_Common_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   printf("Hello, world! I am %d of %d\n", rank, size);
 
